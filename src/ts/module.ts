d13ee809883d099db8763677aa5a60abd31f9288
Ryusei Yamaguchi <mandel59@gmail.com>
2018-10-18 05:14:37 +0900

    modifty declarations

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -23,9 +23,17 @@ namespace Module {
             returnType: ReturnType,
             argTypes: ArgTypes): (...args: { [i in keyof ArgTypes]: NativeJsTypeOf<ArgTypes[i]> }) => NativeJsTypeOf<ReturnType>
 
-    export declare function getValue<T extends ptr<any>>(ptr: ptr<T>, type: "*", noSafe?: boolean): T | 0;
+    export declare function ccall<
+        ReturnType extends Exclude<NativeJsTypeSignature, "array">,
+        ArgTypes extends Array<NativeJsTypeSignature>>(
+            ident: string,
+            returnType: ReturnType,
+            argTypes: ArgTypes,
+            args: { [i in keyof ArgTypes]: NativeJsTypeOf<ArgTypes[i]> }): NativeJsTypeOf<ReturnType>
+
+    export declare function getValue<T extends ptr<any>>(ptr: ptr<T>, type: "*", noSafe?: boolean): T;
     export declare function getValue(ptr: ptr<i32>, type: "i32", noSafe?: boolean): i32;
-    export declare function getValue(ptr: ptr<number>, type: "double", noSafe?: boolean): number;
+    export declare function getValue(ptr: ptr<double>, type: "double", noSafe?: boolean): number;
 
     export declare function UTF8ToString(ptr: ptr<str>): string
     export declare function stringToUTF8(str: string, outPtr: ptr<str>, maxBytesToWrite: number): void
@@ -33,6 +41,6 @@ namespace Module {
     export declare function stackSave(): stack
     export declare function stackRestore(stack: stack): void
     export declare function stackAlloc<T extends sized>(size: number & T["__size__"]): ptr<T>
-    export declare function addFunction<T extends Function>(func: T): ptr<T>
-    export declare function removeFunction<T extends Function>(ptr: ptr<T>): void
+    export declare function addFunction<F extends Function>(func: F): ptr<fn<F>>
+    export declare function removeFunction<F extends Function>(ptr: ptr<fn<F>>): void
 }
