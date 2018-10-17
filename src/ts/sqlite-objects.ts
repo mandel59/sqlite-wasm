2c26cf18da951dd9ba2d115453c70179f080c643
Ryusei Yamaguchi <mandel59@gmail.com>
2018-10-18 04:20:55 +0900

    add types

--- src/ts/sqlite-objects.ts
+++ src/ts/sqlite-objects.ts
@@ -1,4 +1,4 @@
 namespace Module {
-    export type sqlite3_ptr<T> = ptr<T> & { __free__: "sqlite3_free" }
+    export type sqlite3_ptr<T extends { __type__: any }> = ptr<T> & { __free__: "sqlite3_free" }
     export interface sqlite3 { __type__: "sqlite3" }
 }
--- src/ts/type.ts
+++ src/ts/type.ts
@@ -2,8 +2,10 @@ namespace Module {
     export type stack = number & { __type__: "stack" }
     export type i8 = number & { __type__: "i8", __size__: 1 }
     export type i32 = number & { __type__: "i32", __size__: 4 }
-    export type ptr<T> = number & { __type__: "ptr", __deref__: T, __size__: 4 }
+    export type double = number & { __type__: "double", __size__: 8 }
+    export type ptr<T extends { __type__: any }> = number & { __type__: "ptr", __deref__: T, __size__: 4 }
     export type arr<T extends sized> = { __type__: "arr", __deref__: Array<T> }
     export type str = { __type__: "str" }
-    export type sized = { __size__: number }
+    export type fn<F extends Function> = { __type__: "fn", __deref__: F }
+    export type sized = { __type__: unknown, __size__: number }
 }
