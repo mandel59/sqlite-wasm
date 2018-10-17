1d89aa23e740b11a7c9c4f47e0c32065c39c934f
Ryusei Yamaguchi <mandel59@gmail.com>
2018-10-18 04:21:44 +0900

    declare original functions

--- src/ts/sqlite-functions.ts
+++ src/ts/sqlite-functions.ts
@@ -1,4 +1,14 @@
 namespace Module {
+    export declare function _sqlite3_open(filename: ptr<str>, ppDb: ptr<ptr<sqlite3>>): SQLiteResult
+    export declare function _sqlite3_close_v2(pDb: ptr<sqlite3>): SQLiteResult
+    export declare function _sqlite3_exec<T extends ptr<any>>(
+        pDb: ptr<sqlite3>,
+        sql: ptr<str>,
+        callback: ptr<fn<(x: T | 0, numColumns: number, columnTexts: ptr<arr<ptr<str>>>, columnNames: ptr<arr<ptr<str>>>) => i32>> | 0,
+        errmsg: ptr<sqlite3_ptr<str>> | 0
+    ): SQLiteResult
+    export declare function _sqlite3_free(ptr: sqlite3_ptr<any> | 0): void
+
     export const sqlite3_open
         : (filename: string, ppDb: ptr<ptr<sqlite3>>) => SQLiteResult
         = Module["cwrap"]("sqlite3_open", "number", ["string", "number"])
