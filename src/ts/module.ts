50bd3094f7c262278f33396d8f0572ba1ee045b1
Ryusei Yamaguchi <mandel59@gmail.com>
2018-10-18 01:06:21 +0900

    split into files

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,11 +1,4 @@
 namespace Module {
-    export type stack = number & { __type__: "stack" }
-    export type i8 = number & { __type__: "i8", __size__: 1 }
-    export type i32 = number & { __type__: "i32", __size__: 4 }
-    export type ptr<T> = number & { __type__: "ptr", __deref__: T, __size__: 4 }
-    export type arr<T extends sized> = number & { __type__: "arr", __deref__: Array<T>, __size__: 4 }
-    export type sized = { __size__: number }
-
     export declare function getValue<T extends ptr<any>>(ptr: ptr<T>, type: "*", noSafe?: boolean): T | 0;
     export declare function getValue(ptr: ptr<i32>, type: "i32", noSafe?: boolean): i32;
     export declare function getValue(ptr: ptr<number>, type: "double", noSafe?: boolean): number;
---
+++ src/ts/type.ts
@@ -0,0 +1,8 @@
+namespace Module {
+    export type stack = number & { __type__: "stack" }
+    export type i8 = number & { __type__: "i8", __size__: 1 }
+    export type i32 = number & { __type__: "i32", __size__: 4 }
+    export type ptr<T> = number & { __type__: "ptr", __deref__: T, __size__: 4 }
+    export type arr<T extends sized> = number & { __type__: "arr", __deref__: Array<T>, __size__: 4 }
+    export type sized = { __size__: number }
+}
